0fb55389abed72661d545ab13797e6cbc2f95d1d
Sumit112192 <sg0391175@gmail.com>
2021-06-22 13:57:34 +0530

    Modified program to take large password files

--- password_cracker.py
+++ password_cracker.py
@@ -2,18 +2,24 @@ from pikepdf import Pdf
 import sys
 
 def cracking_password(password):
+    i=0
     try:
-        pdf=Pdf.open('PDF.pdf',password)
-        print("Password is "+(password))
-        sys.exit()
-    except:
+        pdf=Pdf.open('Document-protected.pdf',password)
+        sys.stdout.write("\033[K")
         print("[+]-CHECKING-",password)
-        
+        print("Password is", password)
+        i=1
+    except:
+        sys.stdout.write("\033[K")
+        print("[+]-CHECKING-",password, end="\r")
+    if i==1:
+        sys.exit(1)
 
 def cracking():
-    file = open('passwords.txt','r')
-    for line in file.readlines():
-        password = line.split('\n')
-        cracking_password(password[0])
+    with open('passwords.txt', 'r') as f:
+        for line in f:
+            password = line.split('\n')
+            cracking_password(password[0])
+        
 
 cracking()
